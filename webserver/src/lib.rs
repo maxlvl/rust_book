b8a4ef17d504fde0372d32a0896718740ff96ea2
mx <max@maxvl.com>
2022-08-15 23:34:26 -0400

    Multi-threaded webserver proto up and running

---
+++ webserver/src/lib.rs
@@ -0,0 +1,82 @@
+use std::thread;
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::sync::mpsc;
+
+
+trait FnBox {
+    fn call_box(self: Box<Self>);
+}
+
+impl<F: FnOnce()> FnBox for F {
+    fn call_box(self: Box<Self>) {
+        (*self)()
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || {
+            loop {
+                let job = receiver.lock().unwrap().recv().unwrap();
+
+                println!("Worker {} got a job; executing.", id);
+
+                job.call_box()
+            }
+        });
+
+        Worker {
+            id,
+            thread
+        }
+    }
+}
+
+type Job = Box<dyn FnBox + Send + 'static>;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+impl ThreadPool {
+    /// Create a new ThreadPool
+    /// 
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            // create some threads and store them in the vector
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool {
+            workers,
+            sender
+        }
+
+    }
+
+    pub fn execute<F>(&self, f: F)
+        where 
+            F: FnOnce() + Send + 'static
+            {
+                let job = Box::new(f);
+                self.sender.send(job).unwrap();
+            }
+}
