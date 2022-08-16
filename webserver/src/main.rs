b1918574a201fb282683e6ea01566227dc3b2619
mx <max@maxvl.com>
2022-08-15 22:51:56 -0400

    Finish single-threaded webserver

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -15,13 +15,19 @@ fn main() {
         let mut buffer = [0; 1024];
         stream.read(&mut buffer).unwrap();
 
-        let contents = fs::read_to_string("index.html").unwrap();
+        let get = b"GET / HTTP/1.1\r\n";
 
+        let (status_line, filename) = if buffer.starts_with(get) {
+            ("HTTP/1.1 200 OK", "index.html")
+        } else {
+            ("HTTP/1.1 404 NOT FOUND", "404.html")
+        };
+
+        let contents = fs::read_to_string(filename).unwrap();
         let response = format!(
-            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-            contents.len(), contents
-            );
-
+            "{}\r\nContent-Length: {}\r\n\r\n{}",
+            status_line, contents.len(), contents
+        );
         stream.write(response.as_bytes()).unwrap();
         stream.flush().unwrap();
     }
